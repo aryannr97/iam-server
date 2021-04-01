9a3c4e1bfbae43866a1703514209be132e63e76d
Rishabh Nrupnarayan <aryannr97@gmail.com>
2021-04-01 21:53:32 +0530

    [IAM-SERVER]: Swagger API docs added

--- internal/controllers/user-controller.go
+++ internal/controllers/user-controller.go
@@ -23,6 +23,12 @@ type UserController struct {
 }
 
 // AddUser process requests for adding new user
+// @Summary Add user API
+// @Tags user-api
+// @Param request body models.AddUserRequest true "Request Body"
+// @Success 201 {object} models.AddUserResponse
+// @Failure 500 {object} models.RestError
+// @Router /add [post]
 func (uc *UserController) AddUser(c *gin.Context) {
 	var requestPayload models.AddUserRequest
 	if err := c.ShouldBindJSON(&requestPayload); err != nil {
@@ -38,10 +44,16 @@ func (uc *UserController) AddUser(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, res)
+	c.JSON(http.StatusCreated, res)
 }
 
 // GetUser process requests for fetching user details
+// @Summary Get user API
+// @Tags user-api
+// @Param userid path string true "userid"
+// @Success 200 {object} models.UserDetailsResponse
+// @Failure 500 {object} models.RestError
+// @Router /{userid} [get]
 func (uc *UserController) GetUser(c *gin.Context) {
 	userid := c.Param("id")
 	res, err := uc.UserService.GetUser(userid)
@@ -55,6 +67,12 @@ func (uc *UserController) GetUser(c *gin.Context) {
 }
 
 // DeleteUser process requests for deleting user
+// @Summary Delete user API
+// @Tags user-api
+// @Param userid path string true "userid"
+// @Success 204
+// @Failure 500 {object} models.RestError
+// @Router /{userid} [delete]
 func (uc *UserController) DeleteUser(c *gin.Context) {
 	userid := c.Param("id")
 	err := uc.UserService.DeleteUser(userid)
@@ -67,6 +85,13 @@ func (uc *UserController) DeleteUser(c *gin.Context) {
 }
 
 // UpdateUser process requests for updating user details
+// @Summary Update user API
+// @Tags user-api
+// @Param userid path string true "userid"
+// @Param request body models.UpdateUserRequest true "Request Body"
+// @Success 204
+// @Failure 500 {object} models.RestError
+// @Router /{userid} [put]
 func (uc *UserController) UpdateUser(c *gin.Context) {
 	userid := c.Param("id")
 	var requestPayload models.UpdateUserRequest
